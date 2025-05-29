9598cd2b76e44c40d41c798db524d64e4b43ab56
Mark Ke <markke@Marks-MacBook-Air-4.local>
2025-05-29 16:05:03 +0800

    added carouselshow component and added to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import DoctorForm from '../components/DoctorForm'
 import DoctorListPage from './DoctorListPage'
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import CarouselShow from '../components/CarouselShow';
 
 
 export default function Home() {
@@ -12,7 +13,8 @@ export default function Home() {
   
   return (
     <div>
-        <DoctorListPage />
+      <CarouselShow />
+      <DoctorListPage />
     </div>
   )
 }
